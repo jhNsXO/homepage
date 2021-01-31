b4621596af5513e0a820e5ea43e388a7a20c60df
jhNsXO <mrks.jhns@gmail.com>
2021-01-31 22:45:30 +0100

    * init

---
+++ bookmarks.js
@@ -0,0 +1,37 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Disney+", url: "https://disneyplus.com/" },
+      { name: "Amazon Prime", url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Uni",
+    links: [
+      { name: "UGOnline", url: "https://online.uni-graz.at" },
+      { name: "UGemail", url: "https://sbox.edu.uni-graz.at/owa/" },
+      { name: "UGmoodle", url: "https://moodle.uni-graz.at/login/index.php" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Reddit", url: "https://reddit.com/" },
+    ],
+  },
+];
